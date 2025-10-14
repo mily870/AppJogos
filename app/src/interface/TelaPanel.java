40fc4cce670579b16f739bbbe11d5f6319d3bc7c
Kamily Matias <kamily.sonntag.matias@escola.pr.gov.br>
2025-10-14 09:51:26 -0300

    s

---
+++ app/src/interface/TelaPanel.java
@@ -0,0 +1,21 @@
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TelaPanel extends JPanel implements ActionListener{
+    private JPanel telas;
+    private CardLayout controleTela;
+    private JFrame janelas;
+
+    public TelaPanel(JPanel telas, JFrame janelas){
+        this.telas = telas;
+        this.controleTela = (CardLayout) telas.getLayout();
+        this.janelas = janelas;
+        this.setBackground(Color.decode("#202028"));
+
+        imagem botaoFechar = new imagem("");
+    }
+}
